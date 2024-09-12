33332e10b8faea7f3bed1ab3a390e34d36861256
Bongsue Suh <bsuh@kongju.ac.kr>
2024-09-12 09:49:56 +0900

    aaaa

--- src/xxx/xxxx.java
+++ src/xxx/xxxx.java
@@ -4,7 +4,14 @@ public class xxxx {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		fjsldfjlsofjslfjlsfs
+		fjsldfjlsofjslfjlsfs
+		fjsldfjlsofjslfjlsfs
+		fjsldfjlsofjslfjlsfs
+		fjsldfjlsofjslfjlsfs
+		fjsldfjlsofjslfjlsfs
 
 	}
+:w
 
 }
